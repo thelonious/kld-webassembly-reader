1de9c564b651ef2fc60270e3b7085bdf3eed8c93
Kevin Lindsey <kevin@kevlindev.com>
2017-06-11 20:47:21 -0700

    Start disassembling function bodies
    This does not take any operands into account, so we cannot trust the disassembly yet; however, this should cover all opcodes listed in the webassembly docs.

--- sections/CodeSection.js
+++ sections/CodeSection.js
@@ -1,11 +1,40 @@
 let ReadBuffer = require('../ReadBuffer');
 
+let opcodes = require('./opcodes');
+
 class CodeSection {
     read(buffer) {
         let count = buffer.readVarUint(32);
         console.log("count = %d", count);
 
         // TODO: read function bodies
+        for (var i = 0; i < count; i++) {
+            console.log("function body %d", i);
+
+            let body_size = buffer.readVarUint(32);
+            console.log("  body_size = %d", body_size);
+
+            let local_count = buffer.readVarUint(32);
+            console.log("  local_count = %d", local_count);
+
+            let local_entry_count = buffer.readVarUint(32);
+            console.log("  local_entry_count = %d", local_entry_count);
+
+            let local_type = buffer.readVarUint(7);
+            console.log("  local_type = %d", local_type);
+
+            do {
+                var byte = buffer.readUInt8();
+                let mnemonic = opcodes[byte];
+
+                if (mnemonic !== null) {
+                    console.log("  %s", mnemonic);
+                }
+                else {
+                    console.log("  byte = %s", byte.toString(16));
+                }
+            } while (byte !== 0x0b);
+        }
     }
 }
 
